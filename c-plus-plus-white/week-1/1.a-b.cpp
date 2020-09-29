ec02d2be7eca2948ceb527cc7ddd6bcc417ccd9e
Nikita <34914008+Bytan97@users.noreply.github.com>
2020-09-29 19:58:38 +0300

    Create 1.a-b.cpp

---
+++ c-plus-plus-white/week-1/1.a-b.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+  int first_number = 0, second_number = 0;
+
+  std::cin >> first_number >> second_number;
+  std::cout << (first_number + second_number) << std::endl;
+
+  return 0;
+}
