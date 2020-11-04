532ba592e2c076807a7d2d23f757bf25ee3c7dd3
Nikita <34914008+nikitafin@users.noreply.github.com>
2020-11-04 11:57:07 +0300

    finish second week

--- c-plus-plus-white/week-2/10.monthly-affairs.cpp
+++ c-plus-plus-white/week-2/10.monthly-affairs.cpp
@@ -1,53 +1,47 @@
-#include <vector>
-#include <string>
 #include <iostream>
+#include <vector>
 
 int main() {
-  std::vector<std::vector<std::string>> tasks(32);
-  int operations_cnt = 0;
-  int current_month = 1;
-  std::cin >> operations_cnt;
-  for (int i = 0; i < operations_cnt; ++i) {
-    std::string cmd;
-    std::cin >> cmd;
-    if (cmd == "ADD") {
-      int idx = 1;
+  int current_month = 0;
+  std::vector<int> day_in_month = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+  std::vector<std::vector<std::string>> list(day_in_month[current_month]);
+
+  int Q;  // number of commands
+  std::cin >> Q;
+  for (int i = 0; i < Q; ++i) {
+    std::string current_command;
+    std::cin >> current_command;
+    if (current_command == "ADD") {
       std::string task;
-      std::cin >> idx >> task;
-      tasks[idx].push_back(task);
-    } else if (cmd == "DUMP") {
-      int idx = 1;
-      std::cin >> idx;
-      std::cout << tasks[idx].size() << " ";
-      for (const auto &task : tasks[idx]) {
+      int day;
+      std::cin >> day >> task;
+      list[day - 1].push_back(task);
+    } else if (current_command == "DUMP") {
+      int day;
+      std::cin >> day;
+      std::cout << list[day - 1].size() << " ";
+      for (auto task : list[day - 1]) {
         std::cout << task << " ";
       }
-      std::cout << std::endl;
-    } else {
-      current_month = (current_month + 1) % 13;
-      if (current_month == 1 or current_month == 3 or
-          current_month == 5 or current_month == 7 or
-          current_month == 8 or current_month == 12) {
-        tasks.resize(32);
-      } else if (current_month == 4 or current_month == 6
-          or current_month == 9 or current_month == 11) {
-        std::vector<std::string> last = tasks.back();
-        tasks.resize(31);
-        for (const auto &elem : last) {
-          tasks.back().push_back(elem);
+      std::cout << endl;
+    } else if (current_command == "NEXT") {
+      int prev_month = current_month;
+      current_month = (current_month + 1) % 12;
+      int cur_d = day_in_month[current_month];
+      int prev_d = day_in_month[prev_month];
+      if (cur_d < prev_d) {
+        for (int i = cur_d + 1; i <= prev_d; ++i) {
+          if (!list[i - 1].empty()) {
+            list[cur_d - 1].insert(end(list[cur_d - 1]), begin(list[i - 1]),
+                                   end(list[i - 1]));
+          }
         }
+        list.resize(cur_d);
       } else {
-        std::vector<std::string> last = tasks.back();
-        std::vector<std::string> prev_last = tasks[tasks.size() - 2];
-        tasks.resize(29);
-        for (const auto &elem : prev_last) {
-          tasks.back().push_back(elem);
-        }
-        for (const auto &elem : last) {
-          tasks.back().push_back(elem);
-        }
+        list.resize(cur_d);
       }
     }
   }
+
   return 0;
 }
