f5d69b31a1c26e69c9681ba51ebef5583cbaea38
Nikita <34914008+nikitafin@users.noreply.github.com>
2021-03-09 20:57:28 +0300

    finish week-4

---
+++ c-plus-plus-red/week-4/6.server-stats/http_request.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <string_view>
+
+struct HttpRequest {
+  std::string_view method, uri, protocol;
+};
---
+++ c-plus-plus-red/week-4/6.server-stats/stats.cpp
@@ -0,0 +1,44 @@
+#include "stats.h"
+
+void Stats::AddMethod(std::string_view method) {
+  std::string data(method);
+  if (auto it = methodCount.find(data); it != methodCount.end()) {
+    ++methodCount[it->first];
+  } else {
+    ++methodCount["UNKNOWN"];
+  }
+}
+
+void Stats::AddUri(std::string_view uri) {
+  std::string data(uri);
+  if (auto it = uriCount.find(data); it != uriCount.end()) {
+    ++uriCount[it->first];
+  } else {
+    ++uriCount["unknown"];
+  }
+}
+
+const std::map<std::string_view, int> &Stats::GetMethodStats() const {
+  return methodCount;
+}
+
+const std::map<std::string_view, int> &Stats::GetUriStats() const {
+  return uriCount;
+}
+
+HttpRequest ParseRequest(std::string_view line) {
+  // ltrim
+  line.remove_prefix(line.find_first_not_of(" \t\r\f\v\n"));
+  size_t space = line.find(' ');
+  std::string_view method = line.substr(0, space);
+  line.remove_prefix(space + 1);
+
+  space = line.find(' ');
+  std::string_view uri = line.substr(0, space);
+  line.remove_prefix(space + 1);
+
+  space = line.find(' ');
+  std::string_view protocol = line.substr(0, space);
+
+  return {method, uri, protocol};
+}
