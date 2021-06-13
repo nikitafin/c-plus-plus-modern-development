d75c9bf602b968e766292dc823165502e6a3e40e
Nikita Finogenov <finogenov.nik@gmail.com>
2021-06-13 12:00:40 +0300

    todo: 3

---
+++ c-plus-plus-red-v2/week-1/airline_ticket.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <string>
+struct Date {
+  int year, month, day;
+};
+
+struct Time {
+  int hours, minutes;
+};
+
+struct AirlineTicket {
+  std::string from;
+  std::string to;
+  std::string airline;
+  Date departure_date;
+  Time departure_time;
+  Date arrival_date;
+  Time arrival_time;
+  int price;
+};
+
