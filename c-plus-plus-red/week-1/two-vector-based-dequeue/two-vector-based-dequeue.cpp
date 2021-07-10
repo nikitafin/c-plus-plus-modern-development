dee4123f57225ed808fa7889ceb66ab3e529c07f
nikitafin <finogenov.nik@gmail.com>
2021-07-10 12:34:14 +0300

    basic dequeue based on 2 vectors

---
+++ c-plus-plus-red/week-1/two-vector-based-dequeue/two-vector-based-dequeue.cpp
@@ -0,0 +1,15 @@
+#include "dequeue.h"
+
+int
+main()
+{
+  Deque<double> deque;
+  deque.PushBack(123);
+  deque.PushFront(-123);
+  deque.PushBack(123);
+  deque.PushFront(-123);
+  deque.PushBack(123);
+  deque.PushFront(-123);
+  printf_s("123");
+  return EXIT_SUCCESS;
+}
