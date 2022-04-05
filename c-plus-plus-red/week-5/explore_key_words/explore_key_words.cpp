e211f1f8bfdfe225a70015a6394f3d8fcbb4ed99
nikitafin <finogenov.nik@gmail.com>
2022-04-05 21:11:22 +0300

    explore_key_words OK

--- c-plus-plus-red/week-5/explore_key_words/explore_key_words.cpp
+++ c-plus-plus-red/week-5/explore_key_words/explore_key_words.cpp
@@ -1,8 +1,58 @@
 #include "profile.h"
 #include "test_runner.h"
 
+#include <functional>
+#include <future>
+#include <iterator>
 #include <map>
 #include <string>
+#include <thread>
+
+template<typename Iterator>
+class Page
+{
+private:
+  Iterator first, last;
+
+public:
+  Page(Iterator first, Iterator last)
+    : first(first)
+    , last(last)
+  {}
+  size_t size() const { return std::distance(first, last); }
+  auto begin() { return first; }
+  auto end() { return last; }
+  auto begin() const { return first; }
+  auto end() const { return last; }
+};
+
+template<typename Iterator>
+class Paginator
+{
+public:
+  Paginator(Iterator begin, Iterator end, size_t page_size)
+  {
+    size_t container_size = std::distance(begin, end);
+    do {
+      auto page_end = std::next(begin, std::min(page_size, container_size));
+      if (begin != page_end) {
+        elements.push_back({ begin, page_end });
+      }
+      begin = page_end;
+      container_size -= std::min(page_size, container_size);
+    } while (container_size > 0);
+  }
+  size_t size() const { return elements.size(); }
+
+  auto begin() { return elements.begin(); }
+  auto end() { return elements.end(); }
+
+  auto begin() const { return elements.cbegin(); }
+  auto end() const { return elements.cend(); }
+
+private:
+  std::vector<Page<Iterator>> elements;
+};
 
 struct Stats
 {
@@ -16,10 +66,39 @@ struct Stats
   }
 };
 
+template<typename T>
+Stats
+ExploreKeyWordsSingleThread(const T& t, const std::set<std::string>& key_words)
+{
+  Stats stats;
+  for (const auto& s : t) {
+    if (key_words.count(s)) {
+      ++stats.word_frequences[s];
+    }
+  }
+  return stats;
+}
+
 Stats
 ExploreKeyWords(const std::set<std::string>& key_words, std::istream& input)
 {
-  return {};
+  std::vector<std::string> v(
+    std::make_move_iterator(std::istream_iterator<std::string>(input)),
+    std::make_move_iterator(std::istream_iterator<std::string>()));
+
+  Stats result;
+  std::vector<std::future<Stats>> futures;
+  for (auto page : Paginator(
+         v.begin(), v.end(), v.size() / std::thread::hardware_concurrency())) {
+    futures.push_back(std::async(
+      [=]() { return ExploreKeyWordsSingleThread(page, key_words); }));
+  }
+
+  for (auto& f : futures) {
+    result += f.get();
+  }
+
+  return result;
 }
 
 void
