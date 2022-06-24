488eb552b67bbfb5c9c7efb0739da5405db6e03d
nikitafin <finogenov.nik@gmail.com>
2022-06-24 13:05:18 +0300

    start final

---
+++ c-plus-plus-red/final/search_server.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <istream>
+#include <list>
+#include <map>
+#include <ostream>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+
+class InvertedIndex
+{
+public:
+    void Add(const string & document);
+    list<size_t> Lookup(const string & word) const;
+
+    const string & GetDocument(size_t id) const
+    {
+        return docs[id];
+    }
+
+private:
+    map<string, list<size_t>> index;
+    vector<string> docs;
+};
+
+class SearchServer
+{
+public:
+    SearchServer() = default;
+    explicit SearchServer(istream & document_input);
+    void UpdateDocumentBase(istream & document_input);
+    void AddQueriesStream(istream & query_input, ostream & search_results_output);
+
+private:
+    InvertedIndex index;
+};
