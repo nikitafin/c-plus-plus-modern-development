884373e233010428fed07e2b11743abbb3285dc3
nikitafin <finogenov.nik@gmail.com>
2024-08-03 16:19:26 +0300

    refactor: wrap options with "

--- cmake/HandleToolchain.cmake
+++ cmake/HandleToolchain.cmake
@@ -21,29 +21,23 @@ set(CMAKE_CXX_FLAGS_${PROJECT_BUILD_TYPE} "")
 
 # Sanitizers
 if (PROJECT_SANITIZERS)
-    append_compile_option(PROJECT_COMPILE_OPTIONS -g)
-    append_compile_option(PROJECT_COMPILE_OPTIONS -fno-omit-frame-pointer)
+    append_compile_option(PROJECT_COMPILE_OPTIONS "-g")
+    append_compile_option(PROJECT_COMPILE_OPTIONS "-fno-omit-frame-pointer")
 
     # BUG(nikitafin): -fsanitize=address check failed no idea why
-    list(APPEND PROJECT_COMPILE_OPTIONS -fsanitize=${PROJECT_SANITIZERS_STRING})
+    list(APPEND PROJECT_COMPILE_OPTIONS "-fsanitize=${PROJECT_SANITIZERS_STRING}")
     #    append_compile_option(PROJECT_COMPILE_OPTIONS -fsanitize=${PROJECT_SANITIZERS_STRING})
 
-    append_link_option(PROJECT_LINK_OPTIONS -fsanitize=${PROJECT_SANITIZERS_STRING})
+    append_link_option(PROJECT_LINK_OPTIONS "-fsanitize=${PROJECT_SANITIZERS_STRING}")
 endif ()
 
 # Per compiler tune
 if (PROJECT_COMPILER_CLANG)
-    append_compile_option(PROJECT_COMPILE_OPTIONS -static)
+    append_compile_option(PROJECT_COMPILE_OPTIONS "-static")
 
-    append_link_option(PROJECT_LINK_OPTIONS -stdlib=libc++)
+    append_link_option(PROJECT_LINK_OPTIONS "-stdlib=libc++")
 
 endif ()
-
-
-## Some (body)
-#list(SORT PROJECT_COMPILE_OPTIONS)
-#list(SORT PROJECT_COMPILE_DEFINITIONS)
-#list(SORT PROJECT_LINK_OPTIONS)
 
 # Apply
 add_compile_options(${PROJECT_COMPILE_OPTIONS})
